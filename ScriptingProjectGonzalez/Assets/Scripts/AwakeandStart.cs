b749f989efb3eab28a44bde20ebd52ef75241792
Nothim205 <116131975+Nothim205@users.noreply.github.com>
2022-11-14 11:06:56 -0800

    completed scripting project
    completed scripting project with a success

---
+++ ScriptingProjectGonzalez/Assets/Scripts/AwakeandStart.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AwakeandStart : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Awake()
+    {
+        Debug.Log("Awake called.");
+    }
+
+
+    void start()
+    {
+        Debug.Log("Start called.");
+    }
+}
