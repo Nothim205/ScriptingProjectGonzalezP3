c6b0c7fd99f30b728f72a9965b4ed71874f05956
Nothim205 <116131975+Nothim205@users.noreply.github.com>
2022-11-16 10:54:42 -0800

    scripting project finished
    little issue sort of fixed

--- ScriptingProjectGonzalez/Assets/Scripts/AwakeandStart.cs
+++ ScriptingProjectGonzalez/Assets/Scripts/AwakeandStart.cs
@@ -2,14 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AwakeandStart : MonoBehaviour
+public class AwakeAndStart : MonoBehaviour
 {
-    // Start is called before the first frame update
     void Awake()
     {
         Debug.Log("Awake called.");
     }
-
 
     void start()
     {
